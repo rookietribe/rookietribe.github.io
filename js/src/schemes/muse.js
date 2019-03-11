b0258f397380430fdab2b8ace4c8371bd6796d67
rookietribe <kenzor0909@gmail.com>
2019-03-11 11:48:24 +0800

    Site updated: 2019-03-11 11:48:20

---
+++ js/src/schemes/muse.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){function t(){($("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(!0):$(".container").height())<window.innerHeight?$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed"):$("#footer").removeAttr("style position")}t(),$(window).on("resize scroll",t)});
